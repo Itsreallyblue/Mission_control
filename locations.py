e0b617fa20d590be42e3a8d8850898bdda6361ac
Itsreallyblue <100104251+Itsreallyblue@users.noreply.github.com>
2026-07-24 17:32:27 +0100

    added throttle

---
+++ locations.py
@@ -0,0 +1,22 @@
+from pathlib import Path
+import sys
+import os
+
+sys.path.append(str(Path(__file__).resolve().parent))
+
+
+launchpad_locations = [
+    {
+        "name" = "SaxaVord Spaceport",
+        "description" = "Located in Unst, an uninhabitated island in the northernmost part of Scotland on the Lamba Ness peninsula.",
+        "latitude" = 60.82,
+        "longitude" = -0.76,
+    }
+
+    {
+        "name" = "Sutherland Spaceport",
+        "description" = "Melness Crofting Estate, A' Mhòine peninsula, north coast of Scotland",
+        "latitude" = 58.52398,
+        "longitude" = -4.51377,
+    }
+]
